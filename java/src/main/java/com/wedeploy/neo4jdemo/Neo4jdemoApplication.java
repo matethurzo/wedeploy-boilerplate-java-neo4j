c1b2d710b542da996b104ee9a307d4eeaba9fc0a
Máté Thurzó <mate.thurzo@liferay.com>
2018-10-19 13:45:00 +0200

    fixes #1: add minimal spring boot app

---
+++ java/src/main/java/com/wedeploy/neo4jdemo/Neo4jdemoApplication.java
@@ -0,0 +1,12 @@
+package com.wedeploy.neo4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jdemoApplication.class, args);
+	}
+}
